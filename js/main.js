1f33a254d92e076cc1f0f8a60788acb58bda2b5a
Ale <alexlab84@hotmail.com>
2024-06-12 00:11:26 +0200

    I add a guess the number exercise

---
+++ js/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//QUERY SELECTOR
+
+const numberInput = document.querySelector('.number');
+const btn = document.querySelector('.js__btn');
+const pistaEl = document.querySelector('.js__pista');
+const intentosEl = document.querySelector('.js__intentos');
+const counter = document.querySelector('.counter');
+
+
+function getRandomNumber(max) {
+    return parseInt(Math.random() * max);
+   }
+   
+
+   getRandomNumber(100);
+
+   let randomNumber = getRandomNumber(100);
+   
+   console.log(randomNumber)
+   
+btn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    console.log(numberInput.value);
+    increaseCounter();
+    FindNumber(numberInput.value, randomNumber);
+
+    
+});
+
+
+function increaseCounter() {
+    return counter.innerHTML++;
+}
+
+function FindNumber(input, random) {
+    if (numberInput.value > 100 || numberInput.value < 0) {
+        pistaEl.innerHTML = 'El número debe estar entre 1 y 100.';
+    
+    } else {
+        if (numberInput.value > randomNumber) {
+            pistaEl.innerHTML = 'Demasiado alto';  
+        } else if (numberInput.value < randomNumber) {
+            pistaEl.innerHTML = 'Demasiado bajo';
+        
+        } else {
+            pistaEl.innerHTML = 'Has ganado';
+        }
+    }
+}
