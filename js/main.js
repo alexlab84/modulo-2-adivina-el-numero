824d783dc16a1ca8961ccaf2b74045f680cd5057
Ale <alexlab84@hotmail.com>
2024-06-12 19:09:18 +0200

    add teachers corrections in intermediate exercise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//QUERY SELECTOR
 
 const numberInput = document.querySelector('.number');
 const btn = document.querySelector('.js__btn');
@@ -13,20 +12,15 @@ function getRandomNumber(max) {
     return parseInt(Math.random() * max);
    }
    
-
-   getRandomNumber(100);
-
    let randomNumber = getRandomNumber(100);
    
-   console.log(randomNumber)
+   console.log(randomNumber);
    
 btn.addEventListener('click', (ev) => {
     ev.preventDefault();
     console.log(numberInput.value);
     increaseCounter();
-    FindNumber(numberInput.value, randomNumber);
-
-    
+    checkNumber(numberInput.value, randomNumber);
 });
 
 
@@ -34,7 +28,8 @@ function increaseCounter() {
     return counter.innerHTML++;
 }
 
-function FindNumber(input, random) {
+
+function checkNumber(input, random) {
     if (numberInput.value > 100 || numberInput.value < 0) {
         pistaEl.innerHTML = 'El número debe estar entre 1 y 100.';
     
@@ -43,7 +38,7 @@ function FindNumber(input, random) {
             pistaEl.innerHTML = 'Demasiado alto';  
         } else if (numberInput.value < randomNumber) {
             pistaEl.innerHTML = 'Demasiado bajo';
-        
+
         } else {
             pistaEl.innerHTML = 'Has ganado';
         }
